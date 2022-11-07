bb12da399a2f1766abe3d791745aea94f392bfd5
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-07 11:03:52 +0530

    Print Array

---
+++ Generics/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public static class Array
+    {
+        public static void ToPrint(int[] inputArray)
+        {
+            foreach (int Element in inputArray)
+            {
+                Console.WriteLine(Element);
+            }
+            Console.WriteLine("------------------------------------------");
+        }
+        public static void ToPrint(double[] inputArray)
+        {
+            foreach (double Element in inputArray)
+            {
+                Console.WriteLine(Element);
+            }
+            Console.WriteLine("------------------------------------------");
+        }
+        public static void ToPrint(char[] inputArray)
+        {
+            foreach (char Element in inputArray)
+            {
+                Console.WriteLine(Element);
+            }
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] intarray = { 1, 2, 3, 4, 5 };
+            double[] doublearray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] chararray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Array.ToPrint(intarray);
+            Array.ToPrint(doublearray);
+            Array.ToPrint(chararray);
+
+        }
+    }
+}
